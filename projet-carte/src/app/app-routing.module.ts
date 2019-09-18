42e7fb171178d46269f47fd7c03a01075b8187fa
GillesLB <deltag@laposte.net>
2019-09-18 10:41:47 +0200

    Commit routing

--- projet-carte/src/app/app-routing.module.ts
+++ projet-carte/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from 'src/app/accueil/accueil.component';
+import { DetailComponent } from 'src/app/detail/detail.component';
+import { TableauComponent } from 'src/app/tableau/tableau.component';
+import { CarteComponent } from 'src/app/carte/carte.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'carte', component: CarteComponent },
+  { path: 'tableau', component: TableauComponent },
+  { path: 'tableau/:id', component: DetailComponent },
+  { path: '**', component: AccueilComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projet-carte/src/app/app.module.ts
+++ projet-carte/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CarteComponent } from './carte/carte.component';
+import { TableauComponent } from './tableau/tableau.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AccueilComponent,
+    CarteComponent,
+    TableauComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
