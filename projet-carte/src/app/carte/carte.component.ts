bc86d8c9e47351a8b35167ea0b5ae1dd996652ae
GillesLB <deltag@laposte.net>
2019-09-18 12:21:12 +0200

    Commit formulaire ajouter

--- projet-carte/src/app/carte/carte.component.ts
+++ projet-carte/src/app/carte/carte.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+// librairie pour cartes interactives
+import * as L from 'leaflet';
+
+import { Pays } from 'src/app/pays.modele';
 
 @Component({
   selector: 'app-carte',
@@ -7,9 +14,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarteComponent implements OnInit {
 
-  constructor() { }
+  readonly NOM_REGEX = '^[a-zA-Z_-]+$';
+  readonly COORDONNEES_REGEX = '^[0-9,.-]+$';
+
+  ajouterPaysForm: FormGroup;
+
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    // Initialise le formulaire
+    this.ajouterPaysForm = new FormGroup({
+      nom: new FormControl('', [Validators.required, Validators.pattern(this.NOM_REGEX)]),
+      etat: new FormControl('', [Validators.required]),
+      latitude: new FormControl('', [Validators.required, Validators.pattern(this.COORDONNEES_REGEX)]),
+      longitude: new FormControl('', [Validators.required, Validators.pattern(this.COORDONNEES_REGEX)]),
+    });
+
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const myplatonrestau = L.map('cartemonde').setView([47.207527, -1.546276], 2);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Projet Carte'
+    }).addTo(myplatonrestau);
+
+    const origineIcon = L.icon({iconUrl: 'assets/images/marker-icon-laposte.png'});
+    const dejaFaitIcon = L.icon({iconUrl: 'assets/images/marker-icon-blue.png'});
+    const aFaireIcon = L.icon({iconUrl: 'assets/images/marker-icon-blue.png'});
+
+    L.marker([47.207527, -1.546276], {icon: origineIcon}).bindPopup('CSMSI - Atlantica').addTo(myplatonrestau).openPopup();
   }
 
+  onSubmit() {
+    const paysId = this.listePays.length + 1;
+    const nom = this.ajouterPaysForm.get('nom').value;
+    const etat = ''; // this.ajouterPaysForm.get('couleur').value;
+    const latitude = this.ajouterPaysForm.get('latitude').value;
+    const longitude = this.ajouterPaysForm.get('longitude').value;
+    const paysAAjouter = new Pays(paysId, nom, etat, latitude, longitude);
+    // this.liste.push(chienAAjouter);
+    // this.store.dispatch(new AjouterChien(chienAAjouter));
+    this.router.navigate(['carte']);
+  }
 }
---
+++ projet-carte/src/app/pays.modele.ts
@@ -0,0 +1,21 @@
+import { Stream } from 'stream';
+
+export interface IPays {
+  paysId: number;
+  nom: string;
+  etat: string;
+  latitude: number;
+  longitude: number;
+}
+
+export class Pays implements IPays {
+
+  constructor(
+    public paysId: number,
+    public nom: string,
+    public etat: string,
+    public latitude: number,
+    public longitude: number,
+  ) {}
+
+}
